f735645ac6405e2f91e89a195ab573e5a5538cb7
Melissa <melissay94@yahoo.com>
2015-04-19 02:05:54 -0400

    Movement of characters implemented

---
+++ Creature Feature/Assets/Scripts/AnimController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimController : MonoBehaviour {
+
+	public AnimationClip idleAnimation;
+	public AnimationClip rollAnimation;
+	public AnimationClip attackAnimation;
+
+	public float rollMaxAnimationSpeed = 1f;
+
+	private Animation anim;
+
+	enum CharacterState {
+
+		idle = 0,
+		roll = 1,
+		attack = 2,
+	}
+
+	private CharacterState charState;
+
+	public float rollSpeed = 6.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animation> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
---
+++ Creature Feature/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Vector3 moveVector;
+	private CharacterController charControl;
+	private float moveSpeed = 8;
+	private float jumpPower = 15;
+	private float gravity = 30;
+
+	// Use this for initialization
+	void Start () {
+
+		charControl = GetComponent<CharacterController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveVector.x = Input.GetAxis ("LeftJoystickX") * moveSpeed;
+		moveVector.z = -(Input.GetAxis ("LeftJoystickY") * moveSpeed);
+
+		System.Console.WriteLine (moveVector);
+
+			transform.forward = Vector3.Normalize (new Vector3 (-(Input.GetAxis ("LeftJoystickX")), 0f, 
+		                                                   Input.GetAxis ("LeftJoystickY")));
+
+		// Jump Controls
+		if (charControl.isGrounded) {
+
+			moveVector.y = 0;
+
+			if (Input.GetButtonDown("A")) {
+				moveVector.y = jumpPower;
+			}
+		}
+
+		if (Input.GetButtonDown ("B")) {
+			// Where attack logic will go
+		}
+
+		moveVector.y -= gravity * Time.deltaTime;
+		charControl.Move (moveVector * Time.deltaTime);
+
+	}
+}
