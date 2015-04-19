47d965f860e80cefe8e38abd7d76f8c4cef15979
Melissa <melissay94@yahoo.com>
2015-04-19 11:49:55 -0400

    UNITY WORKS ITS ALIIIVE

---
+++ Creature Feature/Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float barDisplay; 
+	public float xPos, yPos;
+
+	private Vector2 pos, size;
+
+	public Texture2D emptyTex, fullTex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		barDisplay = Time.time * 0.05f;    
+	}
+
+	void OnGui() {
+		 
+		// Draw background
+		GUI.BeginGroup (new Rect (pos.x, pos.y, size.x, size.y));
+		GUI.Box (new Rect (0, 0, size.x, size.y), emptyTex);
+
+		//Draw filled-in
+			GUI.BeginGroup (new Rect (0, 0, size.x * barDisplay, size.y));
+				GUI.Box (new Rect (0, 0, size.x, size.y), fullTex);
+			GUI.EndGroup ();
+		GUI.EndGroup ();
+	}
+}
--- Creature Feature/Assets/Scripts/PlayerMovement.cs
+++ Creature Feature/Assets/Scripts/PlayerMovement.cs
@@ -5,41 +5,63 @@ public class PlayerMovement : MonoBehaviour {
 
 	private Vector3 moveVector;
 	private CharacterController charControl;
+	private Animator anim;
 	private float moveSpeed = 8;
 	private float jumpPower = 15;
 	private float gravity = 30;
+	private Vector3 lastRotation;
+
+	public int joystickNum;
 
 	// Use this for initialization
 	void Start () {
 
 		charControl = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator> ();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		moveVector.x = Input.GetAxis ("LeftJoystickX") * moveSpeed;
-		moveVector.z = -(Input.GetAxis ("LeftJoystickY") * moveSpeed);
+		string joystickString = joystickNum.ToString ();
 
-		System.Console.WriteLine (moveVector);
+		moveVector.x = Input.GetAxis ("LeftJoystickX_P" + joystickString) * moveSpeed;
+		moveVector.z = -(Input.GetAxis ("LeftJoystickY_P" + joystickString) * moveSpeed);
 
-			transform.forward = Vector3.Normalize (new Vector3 (-(Input.GetAxis ("LeftJoystickX")), 0f, 
-		                                                   Input.GetAxis ("LeftJoystickY")));
+		if (Input.GetAxis ("LeftJoystickX_P" + joystickString) == 0.0 && Input.GetAxis ("LeftJoystickY_P" + joystickString) == 0.0) {
+			transform.forward = lastRotation;
+		} else {
+			transform.forward = Vector3.Normalize (new Vector3 (-(Input.GetAxis ("LeftJoystickX_P" + joystickString)), 0f, 
+			                                                    Input.GetAxis ("LeftJoystickY_P" + joystickString)));
+		}
+		lastRotation = transform.forward;
 
 		// Jump Controls
 		if (charControl.isGrounded) {
 
 			moveVector.y = 0;
 
-			if (Input.GetButtonDown("A")) {
+			if (Input.GetButtonDown("A_P" + joystickString )) {
 				moveVector.y = jumpPower;
 			}
 		}
 
-		if (Input.GetButtonDown ("B")) {
-			// Where attack logic will go
+		// Attack! Attack! Atttaaaaaaack!!!
+		if (Input.GetButton("B_P" + joystickString)) {
+			anim.SetBool ("attack", true);
+		} 
+		else {
+			anim.SetBool("attack", false);
 		}
+	
+		// Advanced Dodge Slide technique (Aerial mode available)
+		if (Input.GetButton ("X_P" + joystickString)) {
+
+			moveVector.x -= -3 * transform.forward.x * moveSpeed;
+			moveVector.z -= -3 * transform.forward.z * moveSpeed;
+		}
+
 
 		moveVector.y -= gravity * Time.deltaTime;
 		charControl.Move (moveVector * Time.deltaTime);
